400e397f07968bc5f03a2598b75896020569379d
VladimirDo1gov <157319892+VladimirDo1gov@users.noreply.github.com>
2024-12-03 17:45:21 +0400

    1

--- scripts/sudoku/sudoku.mjs
+++ scripts/sudoku/sudoku.mjs
@@ -1,59 +1,96 @@
 import GenerateRandomArrValues from "./randomGenerate.mjs";
-
 class Sudoku {
     constructor() {
         this.cells = document.querySelectorAll(".cell");
-        this.generatorRandom = new GenerateRandomArrValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        this.hiddenCells = {};
         this.buttonResult = document.querySelector(".btn-result");
+        this.buttonNewGame = document.querySelector(".btn-new-game");
+        this.generatorRandom = new GenerateRandomArrValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        this.hiddenCellsHashTable = {};
         this.randomValues = [];
+        this.counterOfCorrectAnswers = 0;
     }
-    fillCellsRandomValues() {
+    makeRanomValues() {
         this.generatorRandom.blenderValues();
         this.randomValues = [...this.generatorRandom.getArr];
+    }
+    fillCellsRandomValues() {
         let i = 0;
-        console.log(this.randomValues);
         this.cells.forEach((cell) => {
             cell.value = this.randomValues[i++];
         });
     }
-    hideCellsValue() {
-        while (Object.keys(this.hiddenCells).length < 2) {
-            let randomIndex = this.generatorRandom.getRandomValue(0, 8);
-            if (!this.hiddenCells[randomIndex]) {
-                this.hiddenCells[randomIndex] = this.cells[randomIndex].value;
+    hideAndSaveRandomCellsValue(n = 2) {
+        while (Object.keys(this.hiddenCellsHashTable).length < n) {
+            let randomIndex = this.generatorRandom.getRandomValue(0, this.randomValues.length - 1);
+            if (!this.hiddenCellsHashTable[randomIndex]) {
+                this.hiddenCellsHashTable[randomIndex] = this.cells[randomIndex].value;
                 this.cells[randomIndex].value = "";
             }
         }
     }
     deleteDisabled() {
-        Object.keys(this.hiddenCells).forEach((key) => {
-            this.cells[key].removeAttribute("disabled");
+        Object.keys(this.hiddenCellsHashTable).forEach((index) => {
+            this.cells[index].removeAttribute("disabled");
+        });
+    }
+    addDisabled() {
+        Object.keys(this.hiddenCellsHashTable).forEach((index) => {
+            this.cells[index].setAttribute("disabled", "true");
         });
     }
 
     checkResult() {
-        let count = 0;
-        Object.keys(this.hiddenCells).forEach((key) => {
-            if (this.cells[key].value == this.hiddenCells[key]) {
-                count++;
+        Object.keys(this.hiddenCellsHashTable).forEach((index) => {
+            if (this.cells[index].value == this.hiddenCellsHashTable[index]) {
+                this.addGreenBorder(index);
+                this.counterOfCorrectAnswers++;
             } else {
-                this.cells[key].classList.add("wrong");
+                this.addRedBorder(index);
             }
         });
-        return count == 2 ? true : false;
+        return this.counterOfCorrectAnswers == 2 ? true : false;
+    }
+
+    addGreenBorder(index) {
+        this.cells[index].classList.add("correct-answer-animation");
+        setTimeout(() => {
+            this.cells[index].classList.remove("correct-answer-animation");
+        }, 500);
+    }
+
+    addRedBorder(index) {
+        this.cells[index].classList.add("incorrect-answer-animation");
+        setTimeout(() => {
+            this.cells[index].classList.remove("incorrect-answer-animation");
+        }, 500);
     }
 
     start() {
+        this.makeRanomValues();
         this.fillCellsRandomValues();
-        this.hideCellsValue();
+        this.hideAndSaveRandomCellsValue(3);
         this.deleteDisabled();
+        console.log(sudoku.hiddenCellsHashTable);
+        console.log(sudoku.randomValues);
         this.buttonResult.onclick = () => {
-            console.log(this.checkResult());
+            this.checkResult();
         };
+        this.buttonNewGame.onclick = () => {
+            this.restart();
+        };
+    }
+
+    reset() {
+        this.addDisabled();
+        this.hiddenCellsHashTable = {};
+        this.randomValues = [];
+        this.counterOfCorrectAnswers = 0;
+    }
+    restart() {
+        this.reset();
+        this.start();
     }
 }
 
 const sudoku = new Sudoku();
 sudoku.start();
-console.log(sudoku.hiddenCells);
